161a22dc537cf2caae95ff4fb435fea2cefa17eb
Simple IP <kaidenjclaflin@gmail.com>
2024-11-23 14:09:55 -0700

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -3,10 +3,10 @@ color a
 title ProjectSolar Module Installer - Initializing...
 
 
-set "version=1.0"
+set "version=1.2.5"
 
 :updateCheck
-curl -o latest_version.txt https://raw.githubusercontent.com/kk123121/ProjectSolar/main/version.txt > nul 2>&1
+curl -o latest_version.txt https://raw.githubusercontent.com/kk123121/ProjectSolar/refs/heads/main/version.txt > nul 2>&1
 set /p latest_version=<latest_version.txt
 del latest_version.txt
 
@@ -19,7 +19,7 @@ if "%latest_version%" GTR "%version%" (
     echo [+] Updating Installer...
     echo.
 	timeout /t 3 > nul
-    curl -o "%~f0.tmp" https://raw.githubusercontent.com/kk123121/ProjectSolar/main/installer.bat > nul 2>&1
+    curl -o "%~f0.tmp" https://raw.githubusercontent.com/kk123121/ProjectSolar/refs/heads/main/installer.bat > nul 2>&1
     move /y "%~f0.tmp" "%~f0" > nul 2>&1
     echo [+] Installer Updated Successfully To Version %latest_version%!
 	echo [+] Press Any Key To Continue...
